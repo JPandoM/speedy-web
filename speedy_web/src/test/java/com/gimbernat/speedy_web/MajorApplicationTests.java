ff68eb2f86135def8b8d5d0cbd57b1c98f28ec06
JPandoM <79473738+JPandoM@users.noreply.github.com>
2021-06-11 16:43:35 +0200

    Holita

---
+++ speedy_web/src/test/java/com/gimbernat/speedy_web/MajorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimbernat.speedy_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
