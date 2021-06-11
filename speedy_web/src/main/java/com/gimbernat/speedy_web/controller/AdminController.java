83ee0c23196e10320dca58ebd7fe77117affd1fd
JPandoM <79473738+JPandoM@users.noreply.github.com>
2021-06-11 17:29:04 +0200

    Without Initial Login

---
+++ speedy_web/src/main/java/com/gimbernat/speedy_web/controller/AdminController.java
@@ -0,0 +1,12 @@
+package com.gimbernat.speedy_web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+	@GetMapping("/admin")
+	public String adminHome() {
+		return "adminHome";
+	}
+}
